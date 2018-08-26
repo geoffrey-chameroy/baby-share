b8b6e18d9a396c734d9aba602c5da2fbd951cecb
geoffrey-chameroy <geoffrey.chameroy@gmail.com>
2018-08-26 14:21:15 +0200

    [feature] As an Admin, i want to enable/disable users

--- src/Controller/Admin/UserController.php
+++ src/Controller/Admin/UserController.php
@@ -4,20 +4,63 @@ namespace App\Controller\Admin;
 
 use App\Service\Manager\UserManager;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends Controller
 {
+    /** @var UserManager */
+    private $userManager;
+    public function __construct(UserManager $userManager)
+    {
+        $this->userManager = $userManager;
+    }
+
     /**
-     * @Route("/users", name="admin_user")
-     * @param UserManager $userManager
+     * @Route("/users", name="admin_user_list")
      * @return Response
      */
-    public function users(UserManager $userManager): Response
+    public function list(): Response
     {
         return $this->render('admin/user/user-list.html.twig', [
-            'users' => $userManager->getList(),
+            'users' => $this->userManager->getList(),
         ]);
+    }
+
+    /**
+     * @Route("/users/enable", name="admin_user_enable")
+     * @param Request $request
+     * @return Response
+     */
+    public function enable(Request $request): Response
+    {
+        $user = $this->userManager->get($request->request->get('id'));
+
+        $token = $request->request->get('token');
+        if ($this->isCsrfTokenValid('admin-user-enable', $token)) {
+            $user->setEnabled(true);
+            $this->userManager->save($user);
+        }
+
+        return $this->redirectToRoute('admin_user_list');
+    }
+
+    /**
+     * @Route("/users/disable", name="admin_user_disable")
+     * @param Request $request
+     * @return Response
+     */
+    public function disable(Request $request): Response
+    {
+        $user = $this->userManager->get($request->request->get('id'));
+
+        $token = $request->request->get('token');
+        if ($this->isCsrfTokenValid('admin-user-disable', $token)) {
+            $user->setEnabled(false);
+            $this->userManager->save($user);
+        }
+
+        return $this->redirectToRoute('admin_user_list');
     }
 }
